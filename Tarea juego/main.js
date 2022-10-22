781f6258c8071c55c0a4d560ae721eecd178595a
Tomas Albornoz <tomas.albnz@gmail.com>
2022-10-21 21:24:38 -0300

    Javatarea

---
+++ Tarea juego/main.js
@@ -0,0 +1,14 @@
+cliente={
+    edad:18,
+    altura:170
+}
+
+if(cliente.edad <18){
+    console. log('Debes tener 18 años o más')
+}else{
+    if(cliente.altura <160){
+        console. log('Debes tener una altura de 160cm o más')
+    } else {
+        console. log('Datos correctos, Puedes hacer fila')
+    }
+}
---
+++ Tarea promedios/main.js
@@ -0,0 +1,6 @@
+const can = 3
+var pro1 = 70
+var pro2 = 30
+var pro3 = 70
+var re = pro1+pro2+pro3
+console. log(re/can)

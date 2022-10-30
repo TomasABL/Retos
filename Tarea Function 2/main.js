22235407710dfec7c57d8d05858bf7f08c8a4478
Tomas Albornoz <tomas.albnz@gmail.com>
2022-10-30 10:50:25 -0300

    Actualizacion2

--- Tarea Function 2/main.js
+++ Tarea Function 2/main.js
@@ -1,22 +1,22 @@
 let valor1 = prompt("ingresa el valor de la base" ,0)
 let valor2 = prompt("ingresa el valor de la altura" ,0)
-function operacione(base, altura, medida,) {
-    let cua = base*altura
+function operaciones(base, altura, medida,) {
+    let cuadrado = base*altura
     if(valor1!=valor2){
         console.log('Para obtener el area de un cuadrado, los valores deben ser iguales')
     } else{
-        console.log(cua, medida, 'area  de cudrado')
+        console.log(cuadrado, medida, 'area  de cudrado')
     }
-    let rec = base*altura
+    let rectangulo = base*altura
     if(valor1==valor2){
         console.log('Para obtener el area de un rectangulo, los valores deben ser distintos')
     }
     else{
-        console.log(rec,medida, 'area de un rectangulo')
+        console.log(rectangulo,medida, 'area de un rectangulo')
     }
-    let tri = base*altura/2
+    let triangulo = base*altura/2
 
   
-    console.log(tri, medida, 'area de un triangulo')
+    console.log(triangulo, medida, 'area de un triangulo')
 }
-operacione(parseInt(valor1),parseInt(valor2), 'cm')
+operaciones(parseInt(valor1),parseInt(valor2), 'cm')
---
+++ Tarea Function 3/main.js
@@ -0,0 +1,12 @@
+function suma(num1,num2,num3,num4,num5) {
+    let resultado =num1+(num1*2)
+    let resultado2=num1+(num2*2)
+    let resultado3=num1+(num3*2)
+    let resultado4=num4+(num5*2)
+    console.log(resultado)
+    console.log(resultado2)
+    console.log(resultado3)
+    console.log(resultado4)
+
+}
+suma(1,3,5,2,6)
